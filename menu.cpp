f66b86cf8abe48b60c221df1fb37cbae064f78ab
Michael-Schneider <Mschneider8@student.rccd,edu>
2024-04-10 00:45:58 -0700

    implemented menu system

---
+++ menu.cpp
@@ -0,0 +1,76 @@
+#include "menu.h"
+
+// blank constructor
+Menu::Menu() : title(""), optionChars(""), gameState(errorState) {}
+
+// fancy constructor
+Menu::Menu(std::string titleIn, std::string optionsIn, std::vector<std::string> &descriptionsIn) 
+: title(titleIn), optionChars(optionsIn)
+{
+    // make sure you didn't mess up on your item counts
+    // idk why the ide is marking this as an error yet. Will see if it compiles later on
+    assert(optionsIn.size() == descriptionsIn.size());
+
+    // deep copy input vector
+    for(int i = 0; i < descriptionsIn.size(); i++)
+    {
+        optionDescriptions.push_back(descriptionsIn[i]);
+    }    
+
+    return;
+}
+
+// prints menu then retrieves option from user
+void Menu::update()
+{
+    do
+    {
+        gameState = menuState;
+        std::cout << std::endl << title << std::endl << std::endl;
+
+        for(int i = 0; i < optionDescriptions.size(); i++)
+        {
+            std::cout << optionChars[i] << ": " << optionDescriptions[i] << std::endl;
+        }
+
+        std::cout << exitState << exitDesc << std::endl << std::endl << "Enter your option: ";
+        gameState = retrieveMenuState();
+    }
+    while(gameState == menuState);
+}
+
+// uses cin or something of that nature to retrieve a menu state, does not let you use invalid ones
+char Menu::retrieveMenuState()
+{
+    std::string input;
+    std::cin >> input;
+
+
+
+    for(int i = 0; i < optionChars.size(); i++)
+    {
+        // checks if input is 1 char and matches an option
+        if(input[0] == optionChars[i])
+        {
+            return optionChars[i];
+        }
+    }
+    
+    // special states
+    if(input[0] == exitState)
+    {
+        return exitState;
+    }
+
+    // invalid response
+    if(gameState == menuState)
+    {
+        std::cout << invalidInputMsg;
+        return menuState;
+    }
+    }
+
+char Menu::getGameState()
+{
+    return gameState;
+}
---
+++ menu.h
@@ -0,0 +1,46 @@
+// menu class for displaying a custom menu to the console
+// and allowing someone to input an option to control the menu
+
+#ifndef menu_h
+#define menu_h
+
+
+#include <vector>
+#include <string>
+#include <cassert>
+#include <iostream>
+
+// two vectors hold data. One is a set of string descriptions the other is a set of char identifiers
+// we also have a title
+// menu sets gamestate which can be read externally
+class Menu
+{
+    private:
+    char retrieveMenuState();
+    
+    public:
+    
+    // constructor 1 lets you pass in a string of char options and a vector of strings to make things easy
+    Menu(std::string titleIn, std::string optionsIn, std::vector<std::string> &descriptionsIn);
+
+    // base constructor lets you set things up as you go instead, leaving everything blank
+    Menu();
+
+    // displays the menu and waits for input
+    void update();
+    char getGameState();
+
+    // data section
+    protected:
+    std::string title;
+    std::string optionChars;
+    std::vector<std::string> optionDescriptions;
+    char gameState;
+    const char exitState = 'e';     // mostly for debugging atm
+    const char errorState = '`';    // sentinel cause ill never use this in menus
+    const char menuState = 'm';     // this will be reserved for the menu
+    const std::string invalidInputMsg = "\nInvalid text received, try again.\n";
+    const std::string exitDesc = ": Exit this menu\n";
+};
+
+#endif
