04b881466eb0fc390a3f6063d9adc57449c68ca9
Michael-Schneider <Mschneider8@student.rccd,edu>
2024-04-11 23:53:49 -0700

    made adjustments to item for the slot functionality.

--- item.h
+++ item.h
@@ -12,7 +12,12 @@ class Item
     std::string getName() {return name;}
     int getLevel() {return level;}
     void print() {std::cout << "\nItem Name: " << name << "\nItem Description: " << description << "\nItem Level: " << level << std::endl;  }
-    
+    int getMaxStack() {return stackSize;}
+    int getID() const {return itemID;}
+    // defualt constructor makes a null Item
+    Item() : name(""), description(""), level(NULL_LEVEL), itemID(NULL_ITEMID), stackSize(0) {}
+
+
     // cast item to bool to test empty
     explicit operator bool() { if(name == "" && description == "" && level == NULL_LEVEL) {return false;} return true; }
 
