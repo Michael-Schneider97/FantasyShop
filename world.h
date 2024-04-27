12bba17a993143b81434ddaac901c275485be119
Michael Schneider <michaews@uci.edu>
2024-04-27 16:22:05 -0700

    Added world class

---
+++ world.h
@@ -0,0 +1,26 @@
+/*
+ * Handles the simulation
+ * Manages time
+ */￼
+ 
+ #include "ItemMasterList.h"
+ 
+ enum class worldState {pause, resume};
+ 
+ class World 
+ {
+	 public:
+	 // call this before doing stuff
+	 bool init();
+	 // starts the simulation(?)
+	 void execute(); 
+	 
+	 /*
+	  * Once theyve been designed
+	  * player, store, npcs will 
+	  * be in the data section below
+	  */
+	  
+	 
+ 
+ };
